b463fae6fb5afebb68b908824fc3a45b68162227
Abdulaziz4010 <abdulazizs707ja@gamil.com>
2024-10-28 09:51:08 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+
+// const arr = [1, 2, 2, 3, 4, 4, 5]
+
+// const newArr = new Set(arr)
+
+// console.log([...newArr]);
+
+
+// const arr = ['apple', 'banana', 'apple', 'orange', 'banana', 'banana'];
+
+// const count = new Map();
+
+// arr.forEach(item => {
+//   count.set(item, (count.get(item) || 0) + 1);
+// });
+
+// console.log(count); 
+
+
+
+
+// const arr = [1, 2, 3, 4]
+
+// const newArr = new Map()
+
+// arr.forEach((item) => {
+//  console.log(item);
+
+//  newArr.set(item, item ** 2)
+ 
+// });
+
+// console.log(newArr);
+
+
+// const array = [1, 9, 2, 3, 2, 4, 1, 11];
+// const count = new Set();
+// const seen = new Set();
+
+// array.forEach(item => {
+//   if (seen.has(item)) {
+//     count.add(item);
+//   } else {
+//     seen.add(item);
+//   }
+// });
+
+// console.log(count); 
+
+
+// const array = [10, 15, 20, 25, 30];
+// const num = 20;
+
+// const result = new Set(array.filter(item => item > num));
+
+// console.log(result); 
+
+
+// const array = ['a', 'b', 'a', 'c', 'b', 'c', 'c'];
+
+// const elementCount = new Map();
+
+// array.forEach(item => {
+//   elementCount.set(item, (elementCount.get(item) || 0) + 1);
+// });
+
+// console.log(elementCount); 
+
+
+// const array = [1, 1, 2, 3, 3, 4, 5, 5];
+// const uniqueArray = Array.from(new Set(array));
+
+// console.log(uniqueArray); 
+
+
+// const keys = ['name', 'age', 'city'];
+// const values = ['John', 30, 'New York'];
+
+// const map = new Map();
+// keys.forEach((key, index) => {
+//   map.set(key, values[index]);
+// });
+
+// console.log(map);
